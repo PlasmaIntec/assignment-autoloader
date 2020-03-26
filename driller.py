7439b7e16a7eaa839753ef583ac6bc7cc89fd095
PlasmaIntec <alphaintec@gmail.com>
2020-03-26 13:29:14 -0700

    Flesh out routine

--- driller.py
+++ driller.py
@@ -3,9 +3,10 @@ from pathlib import Path
 from names import get_first_name, get_last_name
 from selenium.webdriver.common.keys import Keys
 from random import random
-from time import time
+from time import time, sleep
 
 from config import username, password
+from data import data
 
 def load_more(url):
 	crawler = Crawler()
@@ -21,13 +22,56 @@ def load_more(url):
 		submit_button = crawler.findElementByXPath("//*[@id='submitButton']")
 		crawler.click(submit_button)
 
-		new_folder_button = crawler.findElementByXPath("//button[. = 'New Folder']")
-		crawler.click(new_folder_button)
+		for assignment in data:
+			name, due_date = assignment
 
-		name_input = crawler.findElementByXPath("//*[@id='nameId']")
-		name_input.send_keys("Random Name")
-		submission_type_dropdown = crawler.findElementByXPath("//*[@name='SL_SubmissionType']")
-		crawler.click(submission_type_dropdown)
+			new_folder_button = crawler.findElementByXPath("//button[. = 'New Folder']")
+			crawler.click(new_folder_button)
+
+			name_input = crawler.findElementByXPath("//*[@id='nameId']")
+			name_input.send_keys(name)
+			submission_type_dropdown = crawler.findElementByXPath("//option[text()='Observed in person']")
+			submission_type_dropdown.click()
+			completion_type_dropdown = crawler.findElementByXPath("//option[text()='Automatically on evaluation']")
+			completion_type_dropdown.click()
+			category_dropdown = crawler.findElementByXPath("//option[text()='zyBooks Readings']")
+			category_dropdown.click()
+			score_input = crawler.findElementByXPath("//*[@id='z_bu']")
+			score_input.send_keys("10")
+			new_grade_item_button = crawler.findElementByXPath("//a[text()='[New Grade Item]']")
+			new_grade_item_button.click()
+
+			sleep(.5)
+			crawler.switchFrameByXPath("//iframe[@title='New Grade Item']")
+
+			numeric_button = crawler.findElementByXPath("//a[text()='Numeric']")
+			numeric_button.click()
+			grade_name_input = crawler.findElementByXPath("//*[@id='z_c']")
+			grade_name_input.send_keys(name)
+			grade_short_name_input = crawler.findElementByXPath("//*[@id='z_h']")
+			grade_short_name_input.send_keys("zyBooks")
+			grade_category_dropdown = crawler.findElementByXPath("//option[text()='zyBooks (10% of final grade)']")
+			grade_category_dropdown.click()
+
+			crawler.switchToParentFrame()
+			save_button = crawler.findElementByXPath("//td/button[text()='Save']")
+			crawler.click(save_button)
+			restrictions_button = crawler.findElementByXPath("//span[text()='Restrictions']")
+			crawler.click(restrictions_button)
+			has_due_date_checkbox = crawler.findElementByXPath("//*[@id='z_n']")
+			crawler.click(has_due_date_checkbox)
+			due_date_input = crawler.findElementByXPath("//*[@id='z_o']")
+			print(due_date_input.get_attribute('value'))
+			while len(due_date_input.get_attribute('value')) > 0:
+				due_date_input.send_keys(Keys.BACKSPACE)
+			due_date_input.send_keys(due_date)
+			print(due_date_input.get_attribute('value'))
+			due_time_input = crawler.findElementByXPath("//*[@id='z_o$time']")
+			while len(due_time_input.get_attribute('value')) > 0:
+				due_time_input.send_keys(Keys.BACKSPACE)
+			due_time_input.send_keys("8:00 AM")
+			save_and_close_button = crawler.findElementByXPath("//button[text()='Save and Close']")
+			crawler.click(save_and_close_button)
 
 	except Exception as e:
 		print('EXCEPTION', e)
